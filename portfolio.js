9b148ea01038f5bf4eab4ad2024db34c867fc1cf
ryotasawatari <r.sawatari@soa21.jp>
2020-07-03 10:53:52 +0900

    レスポンシブ実装

--- portfolio.js
+++ portfolio.js
@@ -1,27 +1,65 @@
 'use strict';
 
-// const works_panel1 = document.getElementById("works_panel1");
-// const works_panel = document.getElementById("works_panel2");
-// const works_panel3 = document.getElementById("works_panel3");
-// const works_panel4 = document.getElementById("works_panel4");
+// 制作実績詳細の開閉
 
+const works_panel1 = document.getElementById("works_panel1");
+const works_panel2 = document.getElementById("works_panel2");
+const works_panel3 = document.getElementById("works_panel3");
+const works_panel4 = document.getElementById("works_panel4");
 
-// const clickEvent = function clickEvent() {
-//   let works_panel1 = document.getElementById("works_panel1");
-//   if(works_panel1.style.display == "none"){
-//     works_panel1.style.display = "block";
-//   }else{
-//     works_panel1.style.display = "none";
-//   }
-// }
+function click1() {
+  works_panel2.style.display = "none"
+  works_panel3.style.display = "none"
+  works_panel4.style.display = "none"
 
-const openDisplay = document.getElementById("openDisplay");
-
-openDisplay.addEventListener("click",function clickEvent() {
-  const works_panel1 = document.getElementById("works_panel1");
-  if(works_panel1.style.display == "none"){
-    works_panel1.style.display = "block";
-  }else{
-    works_panel1.style.display = "none";
+  if (works_panel1.style.display == "block") {
+    works_panel1.style.display = "none"
+  } else {
+    works_panel1.style.display = "block"
   }
+}
+
+function click2() {
+  works_panel1.style.display = "none"
+  works_panel3.style.display = "none"
+  works_panel4.style.display = "none"
+
+  if (works_panel2.style.display == "block") {
+    works_panel2.style.display = "none"
+  } else {
+    works_panel2.style.display = "block"
+  }
+}
+
+function click3() {
+  works_panel1.style.display = "none"
+  works_panel2.style.display = "none"
+  works_panel4.style.display = "none"
+
+  if (works_panel3.style.display == "block") {
+    works_panel3.style.display = "none"
+  } else {
+    works_panel3.style.display = "block"
+  }
+}
+
+function click4() {
+  works_panel1.style.display = "none"
+  works_panel2.style.display = "none"
+  works_panel3.style.display = "none"
+
+  if (works_panel4.style.display == "block") {
+    works_panel4.style.display = "none"
+  } else {
+    works_panel4.style.display = "block"
+  }
+}
+
+// レスポンシブ用メニュー
+
+$(function() {
+  $(".menu_list").css("display","none");
+  $(".menu_btn").on("click", function() {
+      $(".menu_list").slideToggle('slow');
+  });
 });
